c0f197d29bed89c70125556155580a2c8933b29f
Harish <harish.v@heptagon.in>
2024-01-22 18:55:13 +0530

    creating Zustand store

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useStore} from '../store/store';
 
 const HomeScreen = () => {
+  const coffeeList = useStore((state: any) => state.CoffeeList);
+  const beanList = useStore((state: any) => state.BeanList);
+  console.log(coffeeList);
+  console.log(beanList.length);
+
   return (
     <View>
-      <Text style={{color: '#000'}}>HomeScreen</Text>
+      <Text style={{color: '#000'}}>{coffeeList.length}</Text>
     </View>
   );
 };
